8b9148ce22f90f4c9c2079d6e5485c18bdd5e48a
XielQ <offical.gamerboytr@yandex.com>
2025-06-21 02:00:06 +0300

    feat(functions): ✨ move to_upper and to_lower functions to string method

---
+++ stdlib/ecem2/StringLiteral/lower.hpp
@@ -0,0 +1,14 @@
+#pragma once
+#include <string>
+
+namespace ecem2 {
+namespace StringLiteral {
+inline std::string upper(std::string obj)
+{
+    std::string res = obj;
+    for (auto &c : res)
+        c = tolower(c);
+    return res;
+}
+} // namespace StringLiteral
+} // namespace ecem2
--- stdlib/ecem2/string.hpp
+++ stdlib/ecem2/string.hpp
@@ -7,22 +7,6 @@ inline int strlen(const std::string &str)
     return static_cast<int>(str.length());
 }
 
-inline std::string to_upper(const std::string &s)
-{
-    std::string res = s;
-    for (auto &c : res)
-        c = toupper(c);
-    return res;
-}
-
-inline std::string to_lower(const std::string &s)
-{
-    std::string res = s;
-    for (auto &c : res)
-        c = tolower(c);
-    return res;
-}
-
 inline std::string to_string(int val)
 {
     return std::to_string(val);
