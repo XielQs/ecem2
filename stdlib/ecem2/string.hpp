45be57552fba29e9e9d021d6e67dfe1d866eb87d
XielQ <offical.gamerboytr@yandex.com>
2025-06-23 01:21:32 +0300

    refactor(string): 🔨 remove strlen function

--- stdlib/ecem2/string.hpp
+++ stdlib/ecem2/string.hpp
@@ -2,11 +2,6 @@
 #include <string>
 
 namespace ecem2 {
-inline int strlen(const std::string &str)
-{
-    return static_cast<int>(str.length());
-}
-
 inline std::string to_string(int val)
 {
     return std::to_string(val);
